034bfab2612154e88b7516f1d4ee28d23eb260e8
longtomjr <longtomjr@gmail.com>
2016-09-17 21:59:37 +0200

    Added a abstract Item class and made Weapon inherit it.

---
+++ Assets/Scripts/Models/Items/Item.cs
@@ -0,0 +1,23 @@
+﻿#region LICENSE
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Item.cs" company="SpaceKrakens">
+//   MIT License
+//   Copyright (c) 2016 SpaceKrakens
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+namespace Assets.Scripts.Models.Items
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Base class for all items.
+    /// </summary>
+    public abstract class Item
+    {
+    }
+}
